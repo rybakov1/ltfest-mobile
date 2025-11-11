b9bc0f40cc6435f3c6b19d4cbbc3f79163d5acb2
rybakov1 <rybvlad4@yandex.ru>
2025-11-11 16:36:59 +0300

    improvements

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,18 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+    override func application(
+      _ app: UIApplication,
+      open url: URL,
+      options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      if url.scheme == "ltfestapp" {
+        guard let rootViewController = window?.rootViewController else {
+          return false
+        }
+        rootViewController.dismiss(animated: true, completion: nil)
+      }
+      return super.application(app, open: url, options: options)
+    }
 }
